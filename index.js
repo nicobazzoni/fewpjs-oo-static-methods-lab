b2541b9b8135481c83ae7b286835c39eb58d10d3
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-20 17:52:17 -0400

    commit tree 29c44cff7084742864f9c8daa04d0c1b4ef50449

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word){
+    return word.slice(1) + word[0].toUpperCase() 
+  }
+  static sanitize(word){
+    return word.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize(phrase){
+    const except = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    const res = []
+    const wordArray = phrase.split( " " )
+    for (let n = 0; n < wordArray.length; n++){
+      if( n == 0 ){
+        //capitalize  first word
+        res.push( this.capitalize( wordArray[ n ] ) )
+      }else{
+         // except array includes this word (not the first word)
+        if ( except.includes( wordArray[ n ])){
+          // push that un-capitalized word into the result
+          res.push( wordArray[ n ] )
+        }else{
+          // otherwise capitalize this word and add it to result
+          res.push( this.capitalize( wordArray[ n ] ) )
+        }
+      }
+    }  
+    return res.join(" ")
+  }
 }
